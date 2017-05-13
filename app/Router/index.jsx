7ad0fe5a8b652a3689b50879e69fa32dbcbc04ee
Travis CI <nobody@nobody.org>
2017-05-13 20:48:12 -0300

    Reorganização de estrutura de arquivos

--- app/Router/index.jsx
+++ app/Router/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import App from '../App/index.jsx';
+import Portfolio from '../templates/Portfolio/index.jsx';
 import Home from '../Home/index.jsx';
 import About from '../About/index.jsx';
 
 export default (
-  <App>
+  <Portfolio>
     <Route exact path='/' component={Home} />
     <Route path='/about' component={About} />
-  </App>
+  </Portfolio>
 );
--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -10,7 +10,6 @@ git config --global user.name "Travis CI"
 
 # deploy
 cd public
-cp -r ../images ../styles.css ../index.html .
 git init
 git add .
 git commit -m "Deploy to Github Pages"
