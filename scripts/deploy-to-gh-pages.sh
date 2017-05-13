d81a0195532bf9842f6d6bece79092d6451330e6
Travis CI <nobody@nobody.org>
2017-05-13 19:31:35 -0300

    Nova tentativa de deploy com travis #12

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -13,7 +13,10 @@ git config --global user.name "Travis CI"
 
 # deploy
 cd public
+cp -r ../images ../styles.css ../index.html .
 git init
 git add .
 git commit -m "Deploy to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/erikliberal/erikliberal.github.io.git" master:master > /dev/null 2>&1
+cd ..
+git clean -dfx public
