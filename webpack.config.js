1869a2db6b0ffde09d4237e24be28858f91fc7eb
Travis CI <nobody@nobody.org>
2017-05-13 23:55:55 -0300

    Ajuste para utilizar plugin de copy do webpack para emitir os pacotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
+
 module.exports = {
-    entry: './app/App.jsx',
+    entry: {
+      "bundle":path.join(__dirname,'app','App.jsx')
+    },
     output: {
-        path: [__dirname,'public'].join('/'),
-        filename: 'bundle.js'
+        path:  path.join(__dirname,'public'),
+        filename: '[name].js'
     },
      module: {
          loaders: [{
@@ -10,5 +15,22 @@ module.exports = {
              exclude: /node_modules/,
              loader: 'babel-loader'
          }]
-     }
+     },
+     plugins:[
+       new CopyWebpackPlugin([
+          {context:"pages", from: '**/*', to: ''},
+          {context:"stylesheets", from: '**/*', to: ''},
+          {from: 'images/**/*', to:''},
+          {from: 'data/**/*', to:''},
+          {context: path.join('node_modules','superagent'), from:'superagent.js', to:''}
+/*
+superagent.get('data/posts.json').end(function(err,resp){
+  console.log(err);
+  console.log(resp.body);
+});
+*/
+       ], {
+         copyUnmodified:true
+       })
+     ]
 };
