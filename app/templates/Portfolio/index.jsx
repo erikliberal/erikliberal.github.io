24716f73caeca37b6c9aeb2ddbefb839489e34f5
Travis CI <nobody@nobody.org>
2017-05-15 01:09:26 -0300

    Remoção do footer que não vai mais ser utilizado

--- app/templates/Portfolio/index.jsx
+++ app/templates/Portfolio/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Drawer from './Drawer/index.jsx';
-import Footer from './Footer/index.jsx';
 import Header from './Header/index.jsx';
 
 
@@ -12,7 +11,6 @@ class Portfolio extends React.Component {
           <Drawer />
           <main className="mdl-layout__content">
               {this.props.children}
-              <Footer />
           </main>
           <div className="mdl-layout__obfuscator"></div>
       </div>
